2623dca9174a91dc9d8b40ed85b5b5e079da07aa
Suraj Gudi <gudis488@gmail.com>
2026-02-08 16:26:36 +0530

    Commit

---
+++ TNSjava/src/basic_programs/Demo3.java
@@ -0,0 +1,16 @@
+package basic_programs;
+
+public class Demo3 {
+	public int a=70;  // properties 
+	 String s="Sneha";
+	
+	void drink() {
+		
+	}
+
+	public static void main(String[] args) {
+		Demo3 d=new Demo3();
+		System.out.println(d.a);
+		System.out.println(d.s);
+	}
+}
