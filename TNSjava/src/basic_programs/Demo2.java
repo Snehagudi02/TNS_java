752793c1ea33c79bbd5a2c21113389d226f17aba
Suraj Gudi <gudis488@gmail.com>
2026-02-08 16:19:16 +0530

    Commit

---
+++ TNSjava/src/basic_programs/Demo2.java
@@ -0,0 +1,14 @@
+package basic_programs;
+
+public class Demo2 {
+	public static void main(String[] args) {
+	int $name; 
+	float b=70.6f;   
+	int y=(int)b; 
+	
+	System.out.println("welcome");
+	System.out.println(b);
+	System.out.println(y);
+	
+}
+}
